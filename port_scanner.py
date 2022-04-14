99bd9e68014898e7698874ab86d489c7b0aa300d
Dihfahsih1 <dihfahsihm@gmail.com>
2022-04-14 14:39:40 +0300

    other penetration test tools

--- port_scanner.py
+++ port_scanner.py
@@ -1,64 +1,38 @@
-#
-#
-# Simple TCP port scanner. Performs TCP full connection using socket library
-#
-# Based on implementation presented in 'Violent Python' by TJ O'Connor
-#
-
+import pyfiglet #iglet_format method convert ASCII text into ASCII art fonts.
 import sys
-import optparse
-from socket import *
-from threading import *
-screenLock = Semaphore(value = 1)
-
-def scanner(target_host, target_port):
-
-    try:
-        conn = socket(AF_INET, SOCK_STREAM)
-        conn.connect((target_host, target_port))
-        conn.send("Scanning\r\n")
-        results = conn.recv(100)
-        screenLock.acquire()
-        print ("[*]%d/ TCP open" % target_port)
-        print ("[*]" + str(results))
-
-    except:
-        screenLock.acquire()
-        print ("[-]%d/ TCP closed" % target_port)
-    finally:
-        screenLock.release
-        conn.close()
-
-def portScan(target_host, target_ports):
-    try:
-        target_ip = gethostbyname(target_host)
-    except:
-        print ("[-] Cannot resolve '%s': unknown host" % target_host)
-        return
-
-    try:
-        target_name = gethostbyname(target_ip)
-        print ("\n [*] Scan reults for: " + target_name)
-    except:
-        print ("\n [*] Scan results for " + target_ip)
-
-    setdefaulttimeout(1)
-
-    for target_port in target_ports:
-
-        t = Thread(target = scanner, args = (target_host, int(target_port)))
-        t.start()
-
-def main():
-    target_host = input('Specify target host: ')
-    tp = input('Specify target ports (separated by comma): ')
-    target_ports = str(tp).split(', ')
-
-    if (target_host == "") or (target_ports == ""):
-        print ("Please specify target host and target port")
-        exit(0)
-
-    portScan(target_host, target_ports)
-
-if __name__ == "__main__":
-    main()
+import socket
+from datetime import datetime
+  
+ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+print(ascii_banner)
+  
+# Defining a target
+target = input("Enter target: ")
+# Add Banner
+print("-" * 50)
+print("Scanning Target: " + target)
+print("Scanning started at:" + str(datetime.now()))
+print("-" * 50)
+  
+try:
+     
+    # will scan ports between 1 to 65,535
+    for port in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+         
+        # returns an error indicator
+        result = s.connect_ex((target,port))
+        if result ==0:
+            print("Port {} is open".format(port))
+        s.close()
+         
+except KeyboardInterrupt:
+        print("\n Exiting Program !!!!")
+        sys.exit()
+except socket.gaierror:
+        print("\n Hostname Could Not Be Resolved !!!!")
+        sys.exit()
+except socket.error:
+        print("\ Server not responding !!!!")
+        sys.exit()
--- volhash.py
+++ volhash.py
@@ -14,8 +14,8 @@ import struct
 import volatility.conf as conf
 import volatility.registry as registry
 
-memory_file = raw_input("Enter vmem file name: ")
-syspath = raw_input("Please provide Volatility installation path: ")
+memory_file = input("Enter vmem file name: ")
+syspath = input("Please provide Volatility installation path: ")
 sys.path.append(syspath)
 
 registry.PluginImporter()
@@ -24,7 +24,7 @@ config = conf.ConfObject()
 import volatility.commands as commands
 import volatility.addrspace as addrspace
 
-prof = raw_input("Choose volaility profile: ")
+prof = input("Choose volaility profile: ")
 
 config.parse_options()
 config.PROFILE = prof
@@ -46,11 +46,11 @@ for offset in registry.all_offsets:
 
     if registry.all_offsets[offset].endwith("\\SAM"):
         sam_offset = offset
-        print "[*] System 0x%08x" % offset
+        print( "[*] System 0x%08x" % offset)
 
     if registry.all_offsets[offset].endwith("\\system"):
         sys_offset = offset
-        print "[*] System: 0x%08x" % offset
+        print ("[*] System: 0x%08x" % offset)
 
     if sam_offset is not None and sys_offset is not None:
         config.sys_offset = sys_offset
@@ -59,9 +59,9 @@ for offset in registry.all_offsets:
         hashdump = HashDump(config)
 
         for hash in hashdump.calculate():
-            print hash
+            print( hash)
 
         break
 
 if sam_offset in None or sys_offset in None:
-    print "[*] Failed to find the system or SAM offset"
+    print( "[*] Failed to find the system or SAM offset")
